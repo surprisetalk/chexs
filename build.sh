2de8b5e50a469017b844fba9e28b232bd881d062
Taylor Troesh <surprisetalk@gmail.com>
2023-07-16 06:57:54 -0700

    🕛

--- build.sh
+++ build.sh
@@ -3,13 +3,19 @@
 set -e
 
 mkdir -p dist
+
+ls src
+
 for f_ in src/* ; do
   f=`basename $f_`
   if [[ $f == *.html ]] ; then
-    echo "`cat src/template.html`" "`cat src/$f`"
+    echo
+    echo "===" "src/template.html" "src/$f"
     printf "`cat src/template.html`" "`cat src/$f`"
     printf "`cat src/template.html`" "`cat src/$f`" > dist/$f
   else
     cp src/$f dist/$f
   fi
 done
+
+ls dist
